b46975ecd370fd77f4d0b9f22d1f7a300e1e4d62
Godstime Pious <godstimepious33@gmail.com>
2025-04-23 01:38:23 +0100

    Updated CV Section with a buton and highlight text

--- src/components/CvReview.jsx
+++ src/components/CvReview.jsx
@@ -1,11 +1,11 @@
 import CVReviewImg from "../assets/images/svg/cv-review.svg";
 export default function CvReview() {
   return (
-    <section className="max-w-6xl mx-auto mt-24 w-[90%] ">
+    <section className="max-w-6xl mx-auto mt-24 w-[90%] bg-[#F6F5F0] p-6 mb-12 rounded-lg">
       <div className="flex flex-col gap-12 text-center  sm:text-left lg:flex-row">
-        <div className="">
+        <div className="lg:flex-1/2">
           <div>
-            <h2 className="text-4xl font-bold text-blue-600 mb-10">
+            <h2 className="text-4xl  font-bold text-[#002D6B] mb-10 lg:text-5xl">
               Help get your CV noticed with a CV review!
             </h2>
             <div className="flex flex-col gap-8 ">
@@ -13,7 +13,7 @@ export default function CvReview() {
                 <p className="py-2 px-4 text-white font-bold text-xl bg-amber-500 rounded-full">
                   1
                 </p>
-                <p className="text-xl font-medium text-blue-500">
+                <p className="text-xl  text-[#002D6B]">
                   Submit your CV for review
                 </p>
               </div>
@@ -21,7 +21,7 @@ export default function CvReview() {
                 <p className="py-2 px-4 text-white font-bold text-xl bg-amber-500 rounded-full">
                   2
                 </p>
-                <p className="text-xl font-medium text-blue-500">
+                <p className="text-xl  text-[#002D6B]">
                   Get personalised feedback within 2-3 business days*
                 </p>
               </div>
@@ -30,10 +30,22 @@ export default function CvReview() {
                 <p className="py-2 px-4 text-white font-bold text-xl bg-amber-500 rounded-full">
                   3
                 </p>
-                <p className="text-xl font-medium text-blue-500">
+                <p className="text-xl  text-[#002D6B]">
                   Use the feedback to strengthen your CV!
                 </p>
               </div>
+
+              <a
+                className=" py-4 text-center px-12 bg-emerald-600 text-white font-bold rounded-full"
+                href="#"
+              >
+                Get CV Review
+              </a>
+
+              <small className="text-sm text-blue-700">
+                *In the case of high demand we reserve the right to extend the
+                number of business days to provide personalised feedback.
+              </small>
             </div>
           </div>
         </div>
