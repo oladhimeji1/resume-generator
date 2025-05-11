2f4eda898d8d953b8cce0df43dbf8338fd1e4f34
Ola Abdulrazaq <abdulrazaq2a@gmail.com>
2025-05-11 14:39:27 +0100

    Update the landing page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,94 @@
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 import HeroImg from "../assets/images/hero-img.webp";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 export default function Hero() {
+  var settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+  };
   return (
-    <section
+    <>
+      <Slider {...settings}>
+        <div>
+          <section
+            id="hero"
+            className="flex flex-col-reverse space-y-12 items-center justify-between min-h-screen mx-auto w-[90%] lg:flex-row"
+          >
+            <div className="flex flex-col space-y-8 items-center justify-center lg:items-start lg:w-1/2 ">
+              <h1 className="text-3xl font-bold text-center mb-8 lg:text-left sm:text-5xl leading-normal text-zinc-800">
+                Make your professional resume{" "}
+                <span className="text-emerald-900">in minutes</span>
+              </h1>
+              <p className="text-center text-bold  lg:text-left text-lg text-zinc-600">
+                Build a professional resume that opens doors to new
+                opportunities. Graft a resumse with us that stands out from the
+                crowd, get noticed by employers with a resume that highlights
+                your strengths.
+              </p>
+
+              <Link
+                to="/build-resume"
+                className="bg-emerald-900 px-8 py-4 text-white text-xl rounded-full text-center transition hover:bg-[#5E4AC3]"
+              >
+                Build Your Resume 📃
+              </Link>
+            </div>
+
+            <div>
+              <img
+                src={HeroImg}
+                alt="A simple resume template design with a simple face"
+                loading="eager"
+                className="sm:w-lg"
+              />
+            </div>
+          </section>
+        </div>
+        <div>
+          <section
+            id="hero"
+            className="flex flex-col-reverse space-y-12 items-center justify-between min-h-screen mx-auto w-[90%] lg:flex-row"
+          >
+            <div>
+              <img
+                src={HeroImg}
+                alt="A simple resume template design with a simple face"
+                loading="eager"
+                className="sm:w-lg"
+              />
+            </div>
+
+            <div className="flex flex-col space-y-8 items-center justify-center lg:items-start lg:w-1/2 ">
+              <h1 className="text-3xl font-bold text-center mb-8 lg:text-left sm:text-5xl leading-normal text-zinc-800">
+                Make your professional resume{" "}
+                <span className="text-emerald-900">in minutes</span>
+              </h1>
+              <p className="text-center text-bold  lg:text-left text-lg text-zinc-600">
+                Build a professional resume that opens doors to new
+                opportunities. Graft a resumse with us that stands out from the
+                crowd, get noticed by employers with a resume that highlights
+                your strengths.
+              </p>
+
+              <Link
+                to="/build-resume"
+                className="bg-emerald-900 px-8 py-4 text-white text-xl rounded-full text-center transition hover:bg-[#5E4AC3]"
+              >
+                Build Your Resume 📃
+              </Link>
+            </div>
+          </section>
+        </div>
+      </Slider>
+
+      {/* <section
       id="hero"
       className="flex flex-col-reverse space-y-12 items-center justify-between min-h-screen max-w-7xl mx-auto w-[90%] lg:flex-row"
     >
@@ -29,6 +114,7 @@ export default function Hero() {
       <div>
         <img src={HeroImg} alt="A simple resume template design with a simple face" loading='eager' className="sm:w-lg" />
       </div>
-    </section>
+    </section> */}
+    </>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,25 +7,11 @@ export default function Navbar() {
     setNavBarOpen(!navBarOpen);
   };
   return (
-    <nav className="bg-white h-20 shadow-md p-6 flex items-center justify-between fixed top-0 left-0 right-0 w-full">
-      <Link
-        to="/"
-        className="text-3xl font-[poppins] font-bold text-emerald-900 "
-      >
-        Resume Wizard
-      </Link>
-
-      <button
-        onClick={toggleNavbar}
-        className="text-2xl bg-emerald-900 text-white rounded-md px-4 py-2 flex align-items-center justify-content-center cursor-pointer md:hidden"
-      >
-        &#9776;
-      </button>
-
-      <div className="hidden md:flex space-x-10">
-        <Link to="/" className="hover:text-emerald-900">
+    <nav className="bg-white h-20 shadow-md p-6 flex items-center justify-between fixed top-5 left-0 right-0 w-[90%] mx-auto rounded-full z-[99]">
+      <div className="hidden md:flex space-x-10 font-bold">
+        {/* <Link to="/" className="hover:text-emerald-900">
           Home
-        </Link>
+        </Link> */}
         <Link
           to="/build-resume/resume-templates"
           className="hover:text-emerald-900"
@@ -39,6 +25,32 @@ export default function Navbar() {
           Contact
         </Link>
       </div>
+
+      <Link
+        to="/"
+        className="text-3xl font-[poppins] font-bold text-emerald-900 "
+      >
+        Logo
+      </Link>
+
+      <div className="hidden md:flex justify-center items-center space-x-10">
+        <Link to="#" className="hover:text-emerald-900">
+          Login
+        </Link>
+        <Link
+          to="#"
+          className="p-20 bg-emerald-900 text-white rounded-full px-4 py-2 flex align-items-center justify-content-center cursor-pointer hover:bg-green-800 transition duration-300"
+        >
+          Sign Up
+        </Link>
+      </div>
+
+      <button
+        onClick={toggleNavbar}
+        className="text-2xl bg-emerald-900 text-white rounded-md px-4 py-2 flex align-items-center justify-content-center cursor-pointer md:hidden"
+      >
+        &#9776;
+      </button>
 
       <div
         id="mobile-menu"
