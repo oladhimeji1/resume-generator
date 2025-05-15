be65c9439baa587f5b383279c8a7c209581cb3ab
Godstime Pious <godstimepious33@gmail.com>
2025-05-15 01:09:08 +0100

    Added Download Resume Component, Added a Contact Page

---
+++ src/components/ResumeDownload.jsx
@@ -0,0 +1,54 @@
+import { FaTimes } from "react-icons/fa";
+import { PDFDownloadLink } from "@react-pdf/renderer";
+export default function ResumeDownload({
+  setIsDownloadTemplateShown,
+  resumeData,
+  TemplateComponent,
+}) {
+  return (
+    <div className="h-full w-full z-500 bg-[#7B7B79] fixed inset-0 left-0 right-0 flex items-center justify-center">
+      <div className="max-w-[700px] bg-white relative  min-h-[450px] p-4  rounded-3xl shadow-lg">
+        <FaTimes
+          className="text-xl z-1000 text-emerald-700 cursor-pointer absolute top-5 right-10  md:text-3xl"
+          onClick={() => {
+            setIsDownloadTemplateShown(false);
+          }}
+        />
+        <div className="flex flex-col gap-8">
+          <div>
+            <img
+              src={'/resume-build-illustration.svg'}
+              alt=""
+              className="w-full h-auto max-w-full"
+            />
+          </div>
+          <div className="flex items-center gap-4  justify-between">
+            <div className="flex flex-col gap-4">
+              <h2 className="font-bold text-xl md:text-2xl">Great work!</h2>
+              <p className="text-sm font-medium md:text-lg text-zinc-600">
+                Your resume is really looking good. Click the Download button to
+                save your resume on your local machine for free 🤗
+              </p>
+            </div>
+            <div>
+              {TemplateComponent && (
+                <PDFDownloadLink
+                  document={<TemplateComponent resumeData={resumeData} />}
+                  fileName="myResume.pdf"
+                  className="cursor-pointer"
+                >
+                  <button
+                    type="button"
+                    className="py-4 px-12 bg-emerald-700 cursor-pointer rounded-full text-white font-bold text-xl "
+                  >
+                    Download{" "}
+                  </button>
+                </PDFDownloadLink>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/Template.jsx
+++ src/components/Template.jsx
@@ -1,9 +1,16 @@
-export default function Template({ imageUrl, id, handleTemplateSelect, template }) {
+export default function Template({
+  imageUrl,
+  id,
+  handleTemplateSelect,
+  template,
+}) {
   return (
-    <div className=" shadow-lg cursor-pointer transition hover:-translate-y-5 relative" onClick={()=> {
-      
-      handleTemplateSelect(template)
-    }}>
+    <div
+      className={` shadow-lg cursor-pointer transition hover:-translate-y-5 relative`}
+      onClick={() => {
+        handleTemplateSelect(template);
+      }}
+    >
       <img src={imageUrl} alt={`Template ${id}`} />
     </div>
   );
