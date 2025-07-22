94c8dea7f856b835ba6e462f6f23341572552fe6
Godstime Pious <godstimepious33@gmail.com>
2025-07-22 23:01:09 +0100

    Remove Controls from Testimonial, Fix Typo Error, Fix Bug on ReferenceForm, Updated Site Logo, Fix Layout Tweak in Auth Page

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -26,9 +26,9 @@ export default function Login() {
   return (
     <div className="w-full h-screen px-4 py-2 bg-emerald-900">
       <Link to="/">
-        <img src="/logo_1.png" width="200px" alt="logo" />
+        <img src="/resume_logo_green.jpg" width="200px" alt="logo" />
       </Link>
-      <div className="max-w-[500px] mx-auto bg-white rounded-lg mt-10 shadow-sm">
+      <div className="max-w-[500px] mx-auto bg-white rounded-lg my-3 shadow-sm">
         <form onSubmit={handleSubmit(onSubmit)} className="p-4">
           <div className="header flex items-center flex-col gap-4 justify-center mb-6">
             <h3 className="text-xl font-bold text-center">
